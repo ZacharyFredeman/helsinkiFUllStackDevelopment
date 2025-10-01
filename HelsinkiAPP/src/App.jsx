ac3318c69cc6aab266cb24cb23d7310245a1f842
ZacharyFredeman <zachjfred@gmail.com>
2025-10-01 16:38:10 -0400

    Part 1.5
    completion of challenge 1.5

--- HelsinkiAPP/src/App.jsx
+++ HelsinkiAPP/src/App.jsx
@@ -1,26 +1,30 @@
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name : 'Half Stack application development',
+
+    parts : [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+  
 
   return (
     <div>
-      <Header course = {course} />
-      <Content2 parts = {parts} />
-      <Total total = {parts}/>
+      <Header course = {course.name} />
+      <Content2 parts = {course.parts} />
+      <Total total = {course.parts}/>
     </div>
   )
 }
