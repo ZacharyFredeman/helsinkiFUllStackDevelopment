87c1b3f38e8c39b3c2aebb0665b95cb370a83827
ZacharyFredeman <zachjfred@gmail.com>
2025-11-05 15:06:20 -0500

    Exercises 1.12-1.14 Helsinki fullstack dev course
    Completion of challenges 1.12-1.14 for the helsinki fullstack development course

--- HelsinkiAPP/src/main.jsx
+++ HelsinkiAPP/src/main.jsx
@@ -1,7 +1,8 @@
 import ReactDOM from 'react-dom/client'
-import App from './AppExercises_1.6-1.11'
+import App from './AppExercises_1.12-1.14'
 //AppExercises_1.0-1.6
 //AppExercises_1.6-1.11
+//AppExercises_1.12-1.14
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <App />
