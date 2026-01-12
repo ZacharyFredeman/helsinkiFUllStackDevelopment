ed6473f924bb2268fb243e542b87b05369bbc5f0
ZacharyFredeman <zachjfred@gmail.com>
2026-01-12 12:40:06 -0500

    Completion of section 3.1-3.8
    Completion of exercises 3.1-3.8 of helsinki fullstack development course.

---
+++ Exercises3.1-3.8/index.js
@@ -0,0 +1,118 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+const morgan = require('morgan')
+
+morgan.token('body', requestLogger = (request) => {
+    return((JSON.stringify(request.body)))
+})
+
+//app.use(morgan('tiny'))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
+
+let persons = [
+    { 
+      "id": "1",
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": "2",
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": "3",
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": "4",
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
+
+
+app.get('/', (request, response) => {
+  response.send('<h1>PhoneBook</h1>')
+})
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+app.get('/info', (request, response)=>{
+    var date_time = new Date();
+    response.send(
+        '<div>Phonebook has info for ' + persons.length +' people</div>' +
+        '<div>'+ date_time +'</div>'
+    )
+})
+
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  const person = persons.find(person => person.id === id)
+
+  if(person){
+    response.json(person)
+  }
+  else{
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  persons = persons.filter(person => person.id !== id)
+  response.status(204).end()
+})
+
+const generateId = () => {
+  const maxId = persons.length > 0
+    ? Math.max(...persons.map(n => Number(n.id)))
+    : 0
+  return String(maxId + 1)
+}
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({ 
+      error: 'name or number is missing' 
+    })
+  }
+
+  if(persons.find(person=> person.name === body.name)){
+    return response.status(400).json({ 
+      error: 'name already exist in phone book' 
+    })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  }
+
+  function assignID(req, res, next){
+    req.id = person.id
+    next()
+  }
+  app.use(assignID)
+
+  persons = persons.concat(person)
+
+  response.json(persons)
+})
+
+
